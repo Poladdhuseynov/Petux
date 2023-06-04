7c1b369cf93347d7e1539685ad8f7c415edba166
Poladdhuseynov <135529883+Poladdhuseynov@users.noreply.github.com>
2023-06-04 20:27:28 +0400

    Petux.py
    Petux.py

---
+++ sikdir.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os
+os.system("clear")
+os.system("pkg install figlet")
+os.system("figlet PETUX")
+
+print("""
+
+
+poladin tooludu bu blə ağilli ol:)
+
+
+
+>>>>>>>>>>>>>>>>>by polad<<<<<<<<<<<<<<<<<<<<<
+
+ ay xiyar 1 ə bas yuklənəcək basqa nəyəsə bassan yox:)
+
+""")
+
+reqem=input("dediyimi yaz oxuyub: ")
+if reqem=="1":
+	reqem=input("davam ele xiyar entere bas: ")
+	os.system("git clone https://github.com/sandiwijayani1/spamwa")
+os.system("clear")
+os.system("PETUX")
+
